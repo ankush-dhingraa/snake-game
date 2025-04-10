4abbe798ea1f660ea438d3e80738fa8a3c6cc03e
ankush <ankushkumar20024@gmail.com>
2025-04-10 19:34:34 +0530

    work in  progress

--- main.py
+++ main.py
@@ -8,27 +8,20 @@ screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
+
 snake = Snake()
-# segments = []
-# for i in range(3):
-#     snake_block = Turtle()
-#     snake_block.shape("square")
-#     snake_block.color("white")
-#     snake_block.penup()
-#     segments.append(snake_block)
-# x = -20
-# for t in segments:
-#     t.goto(x,0)
-#     x+=20
+
+screen.listen()
+screen.onkey(key="Right",fun=snake.right)
+screen.onkey(key="Left", fun=snake.left)
+screen.onkey(key="Up", fun=snake.up)
+screen.onkey(key="Down", fun=snake.down)
+
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    # for seg_no in range(len(segments)-1,0,-1):
-    #     new_x = segments[seg_no-1].xcor()
-    #     new_y = segments[seg_no-1].ycor()
-    #     segments[seg_no].goto(new_x,new_y)
-    # segments[0].forward(20)
+
     snake.Move()
 
 
--- snake.py
+++ snake.py
@@ -1,9 +1,14 @@
 from turtle import Turtle
 MOVE_DISTANCE = 25
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
 
     def create_snake(self):
@@ -25,3 +30,16 @@ class Snake:
             new_y = self.segments[seg_no-1].ycor()
             self.segments[seg_no].goto(new_x,new_y)
         self.segments[0].forward(MOVE_DISTANCE)
+    
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
